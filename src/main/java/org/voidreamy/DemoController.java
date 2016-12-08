ef26646ef59becccb4916109774b6a52c495ccf8
voidreamy <voidreamy@gmail.com>
2016-12-08 21:57:39 +0900

    add something

---
+++ src/main/java/org/voidreamy/DemoController.java
@@ -0,0 +1,12 @@
+package org.voidreamy;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
