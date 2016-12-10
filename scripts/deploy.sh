245e2221819039a1ab6138fbe51b232f681fe446
voidreamy <voidreamy@gmail.com>
2016-12-11 01:43:08 +0900

    auto restart

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ARTIFACT="demo-0.0.1-SNAPSHOT.war"
+ssh $DEPLOY_USER@$DEPLOY_HOST "pgrep -f $ARTIFACT | xargs kill"
 ssh $DEPLOY_USER@$DEPLOY_HOST "mv $DEPLOY_PATH/$ARTIFACT $DEPLOY_PATH/$ARTIFACT.old"
 scp "$HOME/build/voidreamy/demo/target/$ARTIFACT" $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
-ssh $DEPLOY_USER@$DEPLOY_HOST "nohup java -jar $DEPLOY_PATH/$ARTIFACT nohup.out &"
+ssh $DEPLOY_USER@$DEPLOY_HOST "nohup java -jar $DEPLOY_PATH/$ARTIFACT > nohup.out 2> nohup.err < /dev/null &"
