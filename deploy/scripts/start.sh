6d23a26c1be206d9ffa4427507236aedf0ac9010
voidreamy <voidreamy@gmail.com>
2016-12-16 23:38:23 +0900

    add codedeploy configuration

--- deploy/scripts/start.sh
+++ deploy/scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup java -jar /home/ec2-user/demo.war >> /home/ec2-user/logs/demo.log 2>> /home/ec2-user/logs/demo_err.log < /dev/null &
+nohup java -jar /home/ec2-user/demo-0.0.1-SNAPSHOT.war >> /home/ec2-user/logs/demo.log 2>> /home/ec2-user/logs/demo_err.log < /dev/null &
--- deploy/scripts/stop.sh
+++ deploy/scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PID=`pgrep -f "java -jar demo.war"`
+PID=`pgrep -f "java -jar demo-0.0.1-SNAPSHOT.war"`
 kill $PID
 if [ $PID ]; then
     while [ kill -0 $PID > /dev/null 2>&1 ]; do
